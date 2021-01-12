25e3923aae7057e5fbd7461491fcba540ea21ba0
Syldori <setokaiba33@gmail.com>
2021-01-12 20:46:48 +0100

    Objetos

--- 05 - Introducción a los objetos.py
+++ 05 - Introducción a los objetos.py
@@ -4,6 +4,7 @@ Objetos.
     -Funciones de primer nivel capaces de llamarse a si mismas y crear copias de si mismas
     -Codigo que encapsulas -tanto el bloque de codigo que hace algo, como tambien las varaibles que en ese codigo luego puedes usar)
     -Ademas, de ese codigo se puden tener muchas copias de el y puede funcionar como objetos
+    -cada objeto tiene unas propiedades y atributos únicos, aunque comparta funcionalidad con otros objetos similares
     
         > por eso siempre se crean con un par de paréntesis y a veces con ciertos parametros
     
@@ -16,7 +17,9 @@ Objetos.
   !!- Es decir, dos objetos pueden tener el mismo código, pero su comportamiento/propiesdades son distintas
         (ejemplo, las dos tortugitas tienen el mismo código de turtle, pero luego una se le da atributos diferentes a la otra (otraTortugita es verde, por ejmplo)
 
-
+    >> el código encapsulado dentro del objeto se llama instancia de un objeto (cada objeto tendrá una instancia distinta, a pesar de que sabemos que tienen la misma funcionalidad)
+   
+   -Los objetos nos permiten lidiar con la complejidad 
 '''
 
 import turtle
